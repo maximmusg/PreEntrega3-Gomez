db3070591762a14e33204dcbc846982e4260b1a9
Gomez Maximiliano <maxil5gomez@gmail.com>
2023-09-24 13:09:07 -0300

    Se agrego perfumes.json

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ class Perfume {
     this.imagen = imagen;
     this.marca = marca;
     this.precio = precio;
-    this.cantidad = 1;
     this.alt = alt;
     this.genero = genero;
     this.descripcion = descripcion;
+    this.cantidad = 1;
   }
   //Para sumar cantidad
   masCantidad() {
@@ -29,140 +29,29 @@ class ControlPerfumes {
   }
 
   //se crearon productos
-  cargarProductos() {
-    const perfum1 = new Perfume(
-      1,
-      "212 Heroes EDT",
-      "img/212l.webp",
-      "Carolina Herrera",
-      66100,
-      "Imagen del perfume",
-      "Masculino",
-      "Siéntete siempre joven con 212 Heroes, un fresco aroma masculino que celebra la juventud, la libertad y la autenticidad. Esta fragancia innovadora y vegana, que viene en un atrevido envase con forma de monopatín representación de un elenco de creativos, es una prueba de que nada es más heroico que ser auténtico."
-    );
-    const perfum2 = new Perfume(
-      2,
-      "Gentleman Society EDP",
-      "img/gent.webp",
-      "Givenchy",
-      87000,
-      "Imagen del perfume",
-      "Masculino",
-      "Givenchy redefine al caballero moderno con su nueva fragancia masculina, Gentleman Society. Una muestra del exclusivo saber hacer de Givenchy, que se compone de materias primas excepcionales. Un Eau de Parfum que reinventa las reglas. Las notas frescas y aromáticas de la Salvia se combinan con el absoluto de flor de Narciso silvestre. Una expresión floral única que contrasta con las facetas oscuras e intensas de un cuarteto de Vetiver de Haití, Uruguay y Madagascar. Las esencias de Cedro y Sándalo se funden con una vainilla adictiva y sensual trazando una inolvidable estela."
-    );
-    const perfum3 = new Perfume(
-      3,
-      "The Only One 2 EDP",
-      "img/dolce.webp",
-      "Dolce & Gabbana",
-      95000,
-      "Imagen del perfume",
-      "Femenino",
-      "El cautivador maridaje entre la violeta y el café se reinterpreta evocando una rosa roja empolvada, como símbolo del amor y la seducción. Las notas de salida de las bayas rojas se combinan con un corazón de aroma de rosas que aporta un aura cálida y sensual a la esencia. Las notas dulces y suaves de las habas tonka en la base aumentan su capacidad adictiva."
-    );
-    const perfum4 = new Perfume(
-      4,
-      "Good Girl EDP",
-      "img/goodgirl.webp",
-      "Carolina Herrera",
-      88500,
-      "Imagen del perfume",
-      "Femenino",
-      "La fragancia GOOD GIRL está inspirada por la visión única de Carolina Herrera de la dualidad de la mujer moderna: audaz y sexy, elegante y enigmática, buena y mala. Siempre forzando y traspasando los límites, la misteriosa sensualidad de GOOD GIRL hace que sea una fragancia moderna y sofisticada a la vez."
-    );
-    const perfum5 = new Perfume(
-      5,
-      "Irresistible EDP RoseVelvet",
-      "img/irres.webp",
-      "Givenchy",
-      97100,
-      "Imagen del perfume",
-      "Femenino",
-      "Una irresistible atracción piel a piel donde la ternura es la nueva adicción. Una fragancia femenina con suculentas grosellas negras, que realzan la feminidad aterciopelada de la rosa, la flor característica de Irresistible. Un Iris que acaricia esta nueva faceta de rosa con su textura empolvada. Una fragancia de mujer cautivante que encarna la gracia empoderada. Irresistible Givenchy invita a dejarse llevar y a ser una misma."
-    );
-    const perfum6 = new Perfume(
-      6,
-      "INVICTUS VICTORY EDP",
-      "img/invictus.webp ",
-      "Dior",
-      118300,
-      "Imagen del perfume",
-      "Masculino",
-      "En este intenso duelo, dos fuerzas se desafían: frescura y sensualidad. Paco Rabanne reúne dos asociaciones exclusivas para garantizar un efecto de larga duración. Una declaración de intenciones sorprendente con un suplemento de frescura: la vivacidad del limón recubierta por un todopoderoso incienso. "
-    );
-    const perfum7 = new Perfume(
-      7,
-      "BLACK XS FOR HER EDP",
-      "img/xsblack.webp ",
-      "Paco Rabanne",
-      44800,
-      "Imagen del perfume",
-      "Femenino",
-      "Black XS for Her, de Paco Rabanne. Siguiendo solo tu instinto. Rompiendo las reglas. Capturando el momento.Black XS for Her: una flor narcótica negra. Sensual, inquietante, rebelde. En exceso."
-    );
-    const perfum8 = new Perfume(
-      8,
-      "ONE MILLION ROYAL EDP",
-      "img/onemillion.webp ",
-      "Paco Rabanne",
-      85150,
-      "Imagen del perfume",
-      "Masculino",
-      "La leyenda renace con una inesperada combinación de revitalizante lavanda, vibrante madera de cedro y sensual benjuí, para una fragancia que es tan desafiante como tú. Una mezcla llamativa de frescura y madera que choca con una sensualidad distintiva para liberar lo que te hace ser únicamente tú."
-    );
-    const perfum9 = new Perfume(
-      9,
-      "212 VIP EDP",
-      "img/212.webp ",
-      "Carolina Herrera",
-      68400,
-      "Imagen del perfume",
-      "Femenino",
-      "212 VIP es una fragancia inspirada en las nuevas generaciones de VIP, mentes creativas que reescriben las reglas de Nueva York con confianza, actitud y personalidad. 212 VIP: una fragancia de personalidad, tanto explosiva como magnética. Una pulsación fragante que combina estilo y actitud, un eau de parfum de fiesta."
-    );
-    const perfum10 = new Perfume(
-      10,
-      " STRONGER WITH YOU EDT",
-      "img/arm.webp",
-      "EMPORIO ARMANI",
-      58900,
-      "Imagen del perfume",
-      "Masculino",
-      "Viví el poder del amor incondicional con el nuevo perfume tendencia de Emporio Armani. Stronger with you, un perfume para vivir emociones intensas. Juntos somos más fuertes."
-    );
-    const perfum11 = new Perfume(
-      11,
-      "Acqua Di Gio EDT",
-      "img/aqua.webp",
-      "Armani",
-      74900,
-      "Imagen del perfume",
-      "Femenino",
-      "Una fragancia deliciosa, como el paraíso en la piel. Un frasco contorneado de vidrio esmerilado verde celadón. Un floral acuático en un cóctel dulce y salado. ACQUA DI GIO es el perfume perfecto para la mujer de espíritu libre, despreocupada y luminosa."
-    );
-    const perfum12 = new Perfume(
-      12,
-      "RALPH'S CLUB EDP",
-      "img/ralp.webp",
-      "Ralph Lauren",
-      106900,
-      "Imagen del perfume",
-      "Masculino",
-      "Ralph's Club de Ralph Lauren es una fragancia de la familia olfativa Amaderada para Hombres. Ralph's Club se lanzó en 2021. La Nariz detrás de esta fragrancia es Dominique Ropion. Las Notas de Salida son esclarea y lavanda; la Nota de Corazón es cedro de Virginia; la Nota de Fondo es vetiver."
-    );
+  async cargarMostrarProductos() {
+    let listaPerfumesJSON = await fetch("perfumes.json");
+    let listaPerfumesJS = await listaPerfumesJSON.json();
 
-    this.agregar(perfum1);
-    this.agregar(perfum2);
-    this.agregar(perfum3);
-    this.agregar(perfum4);
-    this.agregar(perfum5);
-    this.agregar(perfum6);
-    this.agregar(perfum7);
-    this.agregar(perfum8);
-    this.agregar(perfum9);
-    this.agregar(perfum10);
-    this.agregar(perfum11);
-    this.agregar(perfum12);
+    // id, nombre, imagen, marca, precio, alt, genero, descripcion
+
+    listaPerfumesJS.forEach((perfume) => {
+      let nuevoPerfume = new Perfume(
+        perfume.id,
+        perfume.nombre,
+        perfume.imagen,
+        perfume.marca,
+        perfume.precio,
+        perfume.alt,
+        perfume.genero,
+        perfume.descripcion,
+        perfume.cantidad
+      );
+      this.agregar(nuevoPerfume);
+    });
+
+    this.mostrarDOM();
+    // this.eventoFiltro();
   }
 
   //Filtro por genero
@@ -170,6 +59,7 @@ class ControlPerfumes {
     const selectGenero = document.getElementById("select_genero");
     selectGenero.addEventListener("change", () => {
       const generoSeleccionado = selectGenero.value;
+
       this.filtrarPorGenero(generoSeleccionado);
       this.mostrarDOM();
     });
@@ -177,7 +67,7 @@ class ControlPerfumes {
 
   filtrarPorGenero(genero) {
     this.listadoPerfumes = [];
-    this.cargarProductos();
+    // this.cargarMostrarProductos();
     this.listadoPerfumes = this.listadoPerfumes.filter((perfume) => {
       return perfume.genero === genero || genero === "unisex";
     });
@@ -439,14 +329,6 @@ class Carrito {
     }
   }
 
-  // OcultarBotones() {
-  //   if (this.totalCarrito() == 0) {
-  //     vaciarCarritoButton.style.display = "none";
-  //   } else {
-  //     vaciarCarritoButton.style.display = "block";
-  //   }
-  // }
-
   vaciarCarrito() {
     this.listaCarrito = [];
     this.storage();
@@ -485,9 +367,10 @@ class Descripcion {
 const carrito = new Carrito();
 const descripcion = new Descripcion();
 const listp = new ControlPerfumes();
+
 carrito.eFinalizarCompra();
-listp.cargarProductos();
 carrito.getStorage();
 carrito.mostrarDOM();
-listp.mostrarDOM();
+
+listp.cargarMostrarProductos();
 listp.eventoFiltro();
